3ec291639f4dd47f00e6e5e470e27b74b6de9428
Sam Noedel <sam@samnoedel.com>
2014-08-15 21:19:46 -0700

    Adding exception message formatting

---
+++ Lingo.Test/src/ExceptionTest.cs
@@ -0,0 +1,59 @@
+﻿using NUnit.Framework;
+using Lingo;
+using System;
+
+namespace Lingo.Test
+{
+    [TestFixture]
+    public class ExceptionTest : Expectation
+    {
+        [Test]
+        public void ShouldExpandPredicateNullInErrorMessages()
+        {
+            try
+            {
+                Expect(null).To.Equal("foo");
+            }
+            catch (ExpectationException ex)
+            {
+                StringAssert.Contains("null", ex.Message);
+                return;
+            }
+
+            throw new Exception();
+        }
+
+        [Test]
+        public void ShouldExpandExpectationNullInErrorMessages()
+        {
+            try
+            {
+                Expect("foo").To.Equal(null);
+            }
+            catch (ExpectationException ex)
+            {
+                StringAssert.Contains("null", ex.Message);
+                return;
+            }
+
+            throw new Exception();
+        }
+
+        [Test]
+        public void ShouldQuoteStringExpectations()
+        {
+            try
+            {
+                Expect("foo").To.Equal("bar");
+            }
+            catch (ExpectationException ex)
+            {
+                StringAssert.Contains(@"""foo""", ex.Message);
+                return;
+            }
+
+            throw new Exception();
+        }
+    }
+}
+
--- Lingo/src/Predicates/Predicate.cs
+++ Lingo/src/Predicates/Predicate.cs
@@ -8,27 +8,44 @@ namespace Lingo
 
 		protected void Test(bool result, string failureMessage, object expected, object actual)
 		{
+            var expectedFmt = FormatArgument(expected);
+            var actualFmt = FormatArgument(actual);
+
 			if (!Inverted && !result)
 			{
-				var msg = ExpectingExceptionMessage(expected, actual, failureMessage);
+				var msg = ExpectingExceptionMessage(expectedFmt, actualFmt, failureMessage);
 				throw new ExpectationException(msg);
 			}
 			if (Inverted && result)
 			{
-				var msg = NotExpectingExceptionMessage(expected, actual, failureMessage);
+				var msg = NotExpectingExceptionMessage(expectedFmt, actualFmt, failureMessage);
 				throw new ExpectationException(msg);
 			}
 		}
 
-		private string ExpectingExceptionMessage(object expected, object actual, string msg)
+		private string ExpectingExceptionMessage(string expected, string actual, string msg)
 		{
-			return string.Format("Expected {0} to {1} {2}.", expected, msg, actual);
+            return string.Format("Expected {0} to {1} {2}.", expected, msg, actual);
 		}
 
-		private string NotExpectingExceptionMessage(object expected, object actual, string msg)
+		private string NotExpectingExceptionMessage(string expected, string actual, string msg)
 		{
 			return string.Format("Did not expect {0} to {1} {2}", expected, msg, actual);
 		}
+
+        private string FormatArgument(object arg)
+        {
+            if (arg == null)
+            {
+                return "null";
+            }
+            if (arg is string)
+            {
+                return string.Format(@"""{0}""", arg);
+            }
+
+            return arg.ToString();
+        }
 	}
 }
 
